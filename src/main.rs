033b9a06c3d794080ec83b469c8b6a67b57b961e
George Husband <ghus266@gmail.com>
2023-08-29 16:10:03 +0200

    replaced update messages with a match instead of if, working

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,79 @@
 use iced::mouse::Cursor;
+use iced::theme::Theme;
+use iced::widget::canvas::{self, Frame, Geometry, Path, Program};
 use iced::widget::Canvas;
-use iced::{Element, Sandbox, Settings, Color, Length, Point};
-use iced::widget::canvas::{self, Program, Geometry, Frame, Path};
-use iced::theme::Theme; 
+use iced::{Color, Element, Length, Point, Sandbox, Settings};
 
 #[derive(Debug)]
-pub enum Message{
+pub enum Message {
     MouseMoved(Point),
 }
 
-pub fn main()-> iced::Result {
+pub fn main() -> iced::Result {
     Hello::run(Settings::default())
 }
-pub struct Hello{
+pub struct Hello {
     state: Circle, // <- Canvas as a field
 }
 impl Sandbox for Hello {
     type Message = Message;
-    fn new()->Hello{
-        Hello{state: Circle{radius: 50.0, position: Point{x: 0.0, y: 0.0}}}
+    fn new() -> Hello {
+        Hello {
+            state: Circle {
+                radius: 50.0,
+                position: Point { x: 0.0, y: 0.0 },
+            },
+        }
     }
-    fn title(&self)->String{
+    fn title(&self) -> String {
         String::from("Hello")
     }
-    fn update(&mut self, message: Self::Message){
+    fn update(&mut self, message: Self::Message) {
         match message {
-            Message::MouseMoved(x)=>{
-                self.state.position = x
-            }
+            Message::MouseMoved(x) => self.state.position = x,
         }
     }
     fn view(&self) -> Element<'_, Self::Message> {
-        Canvas::new(&self.state).width(Length::Fill).height(Length::Fill).into()
+        Canvas::new(&self.state)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .into()
     }
 }
 // canvas
 #[derive(Debug)]
 struct Circle {
     radius: f32,
-    position: Point
+    position: Point,
 }
 impl Program<Message> for Circle {
     type State = ();
     fn update(
-            &self,
-            _state: &mut Self::State,
-            _event: canvas::Event,
-            bounds: iced::Rectangle,
-            cursor: iced::mouse::Cursor,
-        ) -> (canvas::event::Status, Option<Message>) {
-        if let Some(position) = cursor.position_in(bounds){
-            (canvas::event::Status::Captured, Some(Message::MouseMoved(position)))
-        }else{
-            (canvas::event::Status::Ignored, None)
+        &self,
+        _state: &mut Self::State,
+        _event: canvas::Event,
+        bounds: iced::Rectangle,
+        cursor: iced::mouse::Cursor,
+    ) -> (canvas::event::Status, Option<Message>) {
+        match cursor.position_in(bounds) {
+            Some(position) => (
+                canvas::event::Status::Captured,
+                Some(Message::MouseMoved(position)),
+            ),
+            None => (canvas::event::Status::Ignored, None),
         }
     }
     fn draw(
-            &self, 
-            _state: &Self::State,
-            renderer: &iced::Renderer, 
-            _theme: &Theme, 
-            bounds: iced::Rectangle, 
-            _cursor: Cursor
-        ) -> Vec<Geometry>{
+        &self,
+        _state: &Self::State,
+        renderer: &iced::Renderer,
+        _theme: &Theme,
+        bounds: iced::Rectangle,
+        _cursor: Cursor,
+    ) -> Vec<Geometry> {
         let mut frame = Frame::new(renderer, bounds.size());
-            let circle = Path::circle(self.position, self.radius); 
-            frame.fill(&circle, Color::BLACK); 
+        let circle = Path::circle(self.position, self.radius);
+        frame.fill(&circle, Color::BLACK);
         vec![frame.into_geometry()]
     }
 }
